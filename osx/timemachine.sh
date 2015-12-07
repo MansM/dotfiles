0986ab64e2c5de909cf4e261839b688bfd4401e1
Mans Matulewicz <cybermans@gmail.com>
2015-12-07 15:15:49 +0100

    added save expand by default and Time Machine excludes

---
+++ osx/timemachine.sh
@@ -0,0 +1,8 @@
+# Exclude locations
+sudo tmutil addexclusion ~/.vagrant.d
+sudo tmutil addexclusion ~/.docker
+sudo tmutil addexclusion ~/Downloads
+sudo tmutil addexclusion ~/Library/Application\ Support/Steam/steamapps/
+sudo tmutil addexclusion ~/Movies
+sudo tmutil addexclusion ~/VirtualBox\ VMs
+sudo tmutil addexclusion /opt/homebrew-cask
